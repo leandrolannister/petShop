d34d4e39d16bbeecc474a9018ca1f29641dbfeb4
hendrix <leandrohendrix@gmail.com>
2022-02-03 20:32:32 -0300

    Create Server with Express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-index.js
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is running at 3000');
+});
+
+app.get('/atendimentos', (req,res) => {
+    res.end('You are atendimento route');
+});
