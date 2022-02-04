435c55158c684bee03cfc2f35fcaa9b7b8791d3c
hendrix <leandrohendrix@gmail.com>
2022-02-04 20:29:58 -0300

    Create bodyParser

--- src/app/controllers/atendimentos.js
+++ src/app/controllers/atendimentos.js
@@ -3,4 +3,8 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => {
         res.send('Welcome to route atendimento');
     });
+
+    app.post('/atendimentos', (req,res) => {
+        console.log(req.body);
+    });
 }
--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParse = require('body-parser');
 
 module.exports = () => {
    const app = express();
-
+         app.use(bodyParse.json({extended:true}));         
+         
    consign()
       .include('./src/app/controllers')
       .into(app);
