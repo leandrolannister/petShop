1ff01e8594fc39ad159f467112fa015ad0105c28
hendrix <leandrohendrix@gmail.com>
2022-02-07 08:00:14 -0300

    Connection with MySql

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('./src/config/express.js');
-const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running at 3000');
+const connection = require('./src/infra/connection.js');
+
+connection.connect((error) => {
+    if (error){
+       console.log('Error on connection with mysql');
+    }else {
+       const app = express();
+       app.listen(3000, () => {
+          console.log('Server is running at 3000');
+       });
+    }    
 });
+
+
 
