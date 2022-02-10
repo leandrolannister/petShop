c1055981e5fb9ad1bd782e5c90d85f0bf3ac03aa
hendrix <leandrohendrix@gmail.com>
2022-02-10 07:33:26 -0300

    Data Store

--- src/app/controllers/atendimentos.js
+++ src/app/controllers/atendimentos.js
@@ -1,10 +1,18 @@
 module.exports = app => {
+    let Atendimento = require('../model/Atendimentos.js');
+    let atendimento = new Atendimento();
 
     app.get('/atendimentos', (req, res) => {
         res.send('Welcome to route atendimento');
     });
 
     app.post('/atendimentos', (req,res) => {
-        console.log(req.body);
+        
+        atendimento.store(req.body)
+        .then((success) => {
+            return res.status(200).json(success);
+        }).catch((error) => {
+            return res.status(500).json(error);
+        });       
     });
 }
---
+++ src/app/model/Atendimentos.js
@@ -0,0 +1,20 @@
+class Atendimentos {
+    constructor() {
+        this._connection = require('../../infra/connection.js');
+    }
+
+    store(data) {
+        return new Promise((resolve, reject) => {
+            let query = `INSERT INTO atendimentos(cliente,pet,servico,status,observacoes)
+            VALUES('${data.cliente}','${data.pet}','${data.servico}','${data.status}','${data.observacoes}');`;
+            
+            this._connection.query(query, (error) => {
+                if (error)
+                    reject(`Error on method store: ${error}`);
+                resolve("Atendimento was created successfully");
+            });
+        });
+    }
+}
+
+module.exports = Atendimentos;
